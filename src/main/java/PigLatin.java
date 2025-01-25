f03cfb5087e7d60be355181b207bb18ca7677450
MachineGunWilbur <19082019j@gmail.com>
2025-01-25 00:07:11 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -40,16 +40,31 @@ public class PigLatin {
     }
     }
 
-    public String pigLatin(String sWord) {
-        //precondition: sWord is a valid String of length greater than 0
-        //postcondition: returns the pig latin equivalent of sWord
-        // more code should go here
-	    if(findFirstVowel(sWord) == -1) {
-		    return sWord + "ay";
-	    }
-	    else {
-		return "ERROR!";
-	    }
+    public String pigLatin(String sWord) {  if(sWord.length() == 0){
+  return "";
+}
+if(findFirstVowel(sWord) == -1)
+  {
+    return sWord + "ay";
+  }
+
+  else if(findFirstVowel(sWord) == 0){
+   String str = sWord;
+   str = str + "way";
+   return str;
+  }else if(sWord.substring(0,2).equals("qu")){
+  String str = sWord;
+  str = str.substring(2) + "quay";
+  return str;
+  }else if(findFirstVowel(sWord) != -1){
+    String str = sWord;
+    str = sWord.substring(findFirstVowel(sWord)) + sWord.substring(0,findFirstVowel(sWord)) + "ay";
+    return str;
+  }
+  else
+  {
+    return "ERROR!";
+  }
     }
 }//end PigLatin class
 
