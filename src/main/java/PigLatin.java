172642e868ced9d96d2e9aebbd84d34f5fd835d9
MachineGunWilbur <19082019j@gmail.com>
2025-01-30 10:59:08 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -28,42 +28,36 @@ public class PigLatin {
 	    }
     }
     public int findFirstVowel(String sWord) {
-
-  char [] vowels = {'a', 'e','i','o','u'};
-  int loc = -1;
-  for(int i = 0; i < word.length(); i++){
-    for(int j = 0; j < 5; j++){
-      if(word.charAt(i) == vowels[j]){
-        loc = i;
-        return loc;
-      }
+        //precondition: sWord is a valid String of length greater than 0.
+        //postcondition: returns the position of the first vowel in sWord.  If there are no vowels, returns -1
+	    // your code goes here
+        for(int i = 0; i<sWord.length(); i++){
+    if(sWord.substring(i,i+1).equals("a") || sWord.substring(i,i+1).equals("e") || sWord.substring(i,i+1).equals("i")|| sWord.substring(i,i+1).equals("o")|| sWord.substring(i,i+1).equals("u")){
+      return i;
     }
+  }
+  return -1;
     }
 
     public String pigLatin(String sWord) {
         //precondition: sWord is a valid String of length greater than 0
         //postcondition: returns the pig latin equivalent of sWord
         // more code should go here
- if(sWord.length() == 0){
-  return "";
-}
-if(findFirstVowel(sWord) == -1)
+	   if(findFirstVowel(sWord) == -1)
   {
     return sWord + "ay";
   }
-
-  else if(findFirstVowel(sWord) == 0){
-   String str = sWord;
-   str = str + "way";
-   return str;
-  }else if(sWord.substring(0,2).equals("qu")){
-  String str = sWord;
-  str = str.substring(2) + "quay";
-  return str;
-  }else if(findFirstVowel(sWord) != -1){
-    String str = sWord;
-    str = sWord.substring(findFirstVowel(sWord)) + sWord.substring(0,findFirstVowel(sWord)) + "ay";
-    return str;
+  else if(findFirstVowel(sWord) == 0)
+  {
+    return sWord + "way";
+  }
+  else if(sWord.substring(0,2).equals("qu"))
+  {
+    return sWord.substring(2) + "quay";
+  }
+  else if(findFirstVowel(sWord) != 0)
+  {
+    return sWord.substring(findFirstVowel(sWord)) + sWord.substring(0, findFirstVowel(sWord)) + "ay";
   }
   else
   {
